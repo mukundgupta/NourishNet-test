87b5b982edcf6a84edd5ee573dd345acbdbee446
mukundgupta <mukundgupta2121@gmail.com>
2024-10-15 02:42:14 +0530

    NourishNet-test

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    
+    return render_template("index.html")
+
+@app.route('/page')
+def map_page():
     c = main.main()
-
-    return render_template("page.html", solution = c)
-
-
+    return render_template("page.html",solution=c)
 
 
 if __name__ == '__main__':
